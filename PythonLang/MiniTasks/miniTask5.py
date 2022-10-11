4c492ac7aa0292519e67dc3c4e431f3fe896d71e
Isn0v <isnovikov0@gmail.com>
2022-10-11 23:54:09 +0700

    Create miniTask5.py

---
+++ PythonLang/MiniTasks/miniTask5.py
@@ -0,0 +1,27 @@
+def mult(x, y, z):
+    return x + y + z
+
+#Doesn't work with functions of variable counts of args
+#like mult(*args)
+def specialize(function, *args, **kwargs):
+    args_count = len(args) + len(kwargs.keys())
+    if args_count > function.__code__.co_argcount:
+        raise Exception(f"Got more arguments, than function can allow")
+    elif args_count == function.__code__.co_argcount:
+        def new_function():
+            return function(*args, **kwargs)
+    else:
+        def new_function(*new_args, **new_kwargs):
+            for arg, value in new_kwargs.items():
+                if arg not in function.__code__.co_varnames:
+                    raise Exception(f"{arg}: Function doesn't have such argument(s) name(s)")
+            return function(*new_args, *args, **new_kwargs, **kwargs)
+    for arg, value in kwargs.items():
+        if arg not in function.__code__.co_varnames:
+            raise Exception(f"{arg}: Function doesn't have such argument(s) name(s)")
+    return new_function
+
+#Use variable name starting from right(all args count must be equal to function arg count)
+#or it raises an Exception
+plus_one = specialize(mult, z=1)
+print(plus_one(1, 2)) # >> 4
