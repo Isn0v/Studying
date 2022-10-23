e5749a0fca610006ee66b3789108279dfdefac1e
Isn0v <isnovikov0@gmail.com>
2022-10-23 16:21:14 +0700

    Update miniTask1.py

--- PythonLang/MiniTasks/miniTask1.py
+++ PythonLang/MiniTasks/miniTask1.py
@@ -1,13 +1,14 @@
 num = int(input())
-count = 0 if num > 0 else 1
+count = 0
 
-while 1:
-    if num in [1, -1]:
-        count += 1
-        break
-    if num & 1:
-        count += 1
-    num >>= 1
-
-
-print(count)
+if num == -1:
+    print(2)
+else:
+    while 1:
+        if num in [1, -1]:
+            count += 1
+            break
+        if num & 1:
+            count += 1
+        num >>= 1
+    print(count)
