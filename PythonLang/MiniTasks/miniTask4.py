11c2263eec914e4e05d55a15ae44c8cdc6a1a9b7
Isn0v <isnovikov0@gmail.com>
2022-10-23 16:49:55 +0700

    Update miniTask4.py

--- PythonLang/MiniTasks/miniTask4.py
+++ PythonLang/MiniTasks/miniTask4.py
@@ -1,15 +1,17 @@
-d = {"Ivanov": 97832, "Petrov": [55521], "Kuznecov": 97832, "YA": 97832}
+d = {(1, 2): 97832, "Petrov": 55521, "Kuznecov": 97832, "YA": 97832}
 dict_new = dict()
-
+new_key_value = []
 try:
     for old_key, old_value in d.items():
-        if old_value not in dict_new.keys():
-            dict_new[old_value] = old_key
+        for i in range(len(new_key_value)):
+            if new_key_value[i][0] == old_value:
+                new_key_value[i].append(old_key)
+                break
         else:
-            if type(dict_new[old_value]) == str:
-                dict_new[old_value] = [dict_new[old_value], old_key]
-            else:
-                dict_new[old_value] = [*dict_new[old_value], old_key]
+            new_key_value.append([old_value, old_key])
+    for lst in new_key_value:
+        dict_new[lst[0]] = lst[1:]
+
 except Exception:
     print("Error")
 
