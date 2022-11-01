a9a1beb058b0909256b62a6a8f196a3746402a92
Isn0v <isnovikov0@gmail.com>
2022-11-01 23:59:42 +0700

    Fixing miniTask7

--- PythonLang/MiniTasks/miniTask7.py
+++ PythonLang/MiniTasks/miniTask7.py
@@ -1,18 +1,17 @@
 def flatten(lst: list, depth=-1) -> list:
+    
     res_lst = []
     for i in range(len(lst)):
-        if not isinstance(lst[i], list):
+        if not isinstance(lst[i], list) or depth == 0:
             res_lst.append(lst[i])
         else:
             if depth == -1:
                 res_lst = [*res_lst, *flatten(lst[i])]
             elif depth > 0:
                 res_lst = [*res_lst, *flatten(lst[i], depth - 1)]
-            else:
-                res_lst = [*res_lst, lst[i]]
 
     return res_lst
 
 
 lst = [1, 2, [4, 5], [6, [7]], 8]
-print(flatten(lst, 1))
+print(flatten(lst))
