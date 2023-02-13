83aebd6fedcf8138bd7a90437b1f409702b32dce
Isn0v <isnovikov0@gmail.com>
2023-02-13 16:35:49 +0700

    miniTask4 for Algos done

---
+++ Algorithms and Structures/miniTask3.py
@@ -0,0 +1,21 @@
+def search(nums, target):
+    left = 0
+    right = len(nums)
+    res_ind = -1
+    it_ind = right // 2
+    while True:
+        if left >= right - 1 and nums[it_ind] != target:
+            break
+        if nums[it_ind] == target:
+            res_ind = it_ind
+            break
+        elif target < nums[it_ind]:
+            right = it_ind
+        elif target > nums[it_ind]:
+            left = it_ind
+        it_ind = (left + right) // 2
+    return res_ind
+
+
+lst = [1,4,8,9,14,18,19,65]
+print(search(lst, 20))
