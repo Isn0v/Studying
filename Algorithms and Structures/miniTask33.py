c40e090ff21fbc983a8ca2a7982ecd2ce02c118f
Isn0v <isnovikov0@gmail.com>
2023-09-05 17:08:35 +0700

    miniTask32, miniTask33 done

---
+++ Algorithms and Structures/miniTask33.py
@@ -0,0 +1,11 @@
+prices = [7,1,5,3,6,4]
+
+def maxProfit(prices):
+    profit = 0
+    min_n = prices[0]
+    for i in range(1, len(prices)):
+        min_n = min(min_n, prices[i])
+        if prices[i] - min_n > 0:
+            profit += prices[i] - min_n
+            min_n = prices[i]
+    return profit
