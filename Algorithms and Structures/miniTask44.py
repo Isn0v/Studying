cb68c4f9c20a05784344901b478b645f5d2a964e
Isn0v <isnovikov0@gmail.com>
2023-12-12 22:22:30 +0700

    miniTask44 done

---
+++ Algorithms and Structures/miniTask44.py
@@ -0,0 +1,26 @@
+from typing import List
+
+def createSortedArray(instructions: List[int]) -> int:
+    m = max(instructions)
+    c = [0] * (m + 1)
+
+    def update(x):
+        while (x <= m):
+            c[x] += 1
+            x |= x + 1
+
+    def get(x):
+        res = 0
+        while (x > 0):
+            res += c[x]
+            x = (x & (x + 1)) - 1
+        return res
+
+    res = 0
+    for i, a in enumerate(instructions):
+        res += min(get(a - 1), i - get(a))
+        update(a)
+    return res % (10**9 + 7)
+
+if __name__ == "__main__":
+    print(createSortedArray([1,3,3,3,2,4,2,1,2]))
