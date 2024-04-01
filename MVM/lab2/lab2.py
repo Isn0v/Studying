f1ee44c95de0ef7ff3ed21c83db9423f3abfdf02
Isn0v <isnovikov0@gmail.com>
2024-04-01 22:23:51 +0700

    implementation of square matrix in NSTT3

--- MVM/lab2/lab2.py
+++ MVM/lab2/lab2.py
@@ -1,134 +1,196 @@
-import sys, cmath, sympy
+import sys
+import math
+import sympy
 import matplotlib.pyplot as plt
 import numpy as np
 from functools import partial
 
+
 def df_simple(f, x, epsilon=sys.float_info.epsilon):
-    #five-point stencil method
+    # five-point stencil method
     return (-f(x + 2 * epsilon) + 8 * f(x + epsilon) - 8 * f(x - epsilon) + f(x - 2 * epsilon)) / (12 * epsilon)
+
 
 def func_to_solve(x):
     return sympy.tan(x) - x
 
-def bisection_solution(f, a, b, epsilon=sys.float_info.epsilon):
+
+def bisection_solution(f, n, interval=(-math.pi/2, math.pi/2), epsilon=sys.float_info.epsilon):
+    # a, b = interval[0] + math.pi*n + epsilon, interval[1]*n + math.pi*n - epsilon
+    a = np.float64(interval[0] + math.pi*n + epsilon)
+    b = np.float64(interval[1] + math.pi*n - epsilon)
+    iterations = 0
+    
     if f(a) * f(b) >= 0:
         print("Bisection method fails.")
         return None
     c = a
     while (b - a) / 2 >= epsilon:
         c = (a + b) / 2
+        iterations += 1
         if f(c) == 0.0:
             break
         if f(a) * f(c) < 0:
             b = c
         else:
             a = c
-    return c
+    return c, iterations
 
-def simple_iteration_solution(f, x0, epsilon=sys.float_info.epsilon):
-    x = np.float64(x0)
+
+def simple_iteration_solution(f, n, interval=(-math.pi/2, math.pi/2), epsilon=sys.float_info.epsilon):
+    
+    a = np.float64(interval[0] + math.pi*n + epsilon)
+    b = np.float64(interval[1]*n + math.pi*n - epsilon)
     
     x_symb = sympy.Symbol('x')
     df_symb = sympy.diff(f(x_symb))
     df = sympy.lambdify(x_symb, df_symb, 'numpy')
-    if df(x0) >= 1:
-        print("Simple iteration method fails.")
-        return None
     
+    lin_sp = np.linspace(a, b, 50)
+    
+    x0 = 0
+    iterations = 0
+    for x in lin_sp:
+        x0 = x
+        if df(x0) < 1:
+            break
+
     lambda0 = df(x0)
-    
+
     while True:
-        print(x)
+        # print(x)
+        iterations += 1
+        
         x = x - lambda0 * f(x)
         if abs(x - x0) < epsilon:
             break
         x0 = x
-    return x
+        
+    return x, iterations
+
 
 def get_tangent_f(f, df, x0):
     def tangent(x):
         return f(x0) + df(x0) * (x - x0)
-    
+
     return tangent
 
-def secant_method_solution(f, x0, x1, epsilon=sys.float_info.epsilon):
-    x = np.float64(x1)
+
+# def secant_method_solution(f, x0, x1, epsilon=sys.float_info.epsilon):
+def secant_method_solution(f, n, interval=(-math.pi/2, math.pi/2), epsilon=sys.float_info.epsilon, interval_division_points=20):
+    # x0 -> starting point
+    # x1 -> point to be changed
+    a = np.float64(interval[0] + math.pi*n + epsilon)
+    b = np.float64(interval[1] + math.pi*n - epsilon)
+    
+    sign_n = -1 if n < 0 else 1
+    
+    lin_sp = np.linspace(a, b, interval_division_points)
+    iterations = 0
+    x0 = lin_sp[-1*sign_n]
+    x = lin_sp[-2*sign_n]
 
     while True:
         # print(x)
         x_temp = x
         x = x - f(x) * (x - x0) / (f(x) - f(x0))
         x0 = x_temp
+        
+        iterations += 1
+        
         if abs(x - x0) < epsilon:
             break
-    return x
+            
+    return x, iterations
+
+
 def z_function(z):
-    return z**3 - complex(1,0)
+    return z**3 - complex(1, 0)
 
 
-particular_point = np.complex128(0.8 + -0.8j)
+particular_point = np.complex128(0.4 + -0.5j)
 convergent_roots = [particular_point]
 
-def newton_method_solution(f, x0, epsilon=sys.float_info.epsilon, dtype=np.float64, show_convergence=False):
-    x = dtype(x0)
+
+def newton_method_solution(f, n=0, interval=(-math.pi/2, math.pi/2), z0=0, epsilon=sys.float_info.epsilon, dtype=np.float64, show_convergence=False):
+
     
-    path_point_convergence_flag = False
-    
-    num_to_round = 4
-    
-    if dtype == np.complex128:
-        round_x0 = np.complex128(round(x0.real, num_to_round) + round(x0.imag, num_to_round)*1j)
-        round_convergent_point = np.complex128([round(particular_point.real, num_to_round) + round(particular_point.imag, num_to_round)*1j])
-    else:
-        round_x0 = round(x0, num_to_round)
-        round_convergent_point = round(particular_point, num_to_round)
-    
-    
-    if round_x0 == round_convergent_point:
-        path_point_convergence_flag = True
+    a = np.float64(interval[0] + math.pi*n + epsilon)
+    b = np.float64(interval[1]*n + math.pi*n - epsilon)
+
+    lin_sp = np.linspace(a, b, 50)
     
     x_symb = sympy.Symbol('x')
     df_symb = sympy.diff(f(x_symb))
     df = sympy.lambdify(x_symb, df_symb)
-    
+
     d2f_symb = sympy.diff(f(x_symb), x_symb, x_symb)
     d2f = sympy.lambdify(x_symb, d2f_symb)
     
-    # if abs(f(x0)*d2f(x0)) >= abs(df(x0)**2): почему-то не работает корректно
+    x0 = 0
+    path_point_convergence_flag = False
+    
+    if dtype != np.complex128:
+        for x in lin_sp:
+            if abs(f(x0)*d2f(x0)) < abs(df(x0)**2):
+                x0 = x
+                break
+    else:    
+
+        num_to_round = 4
+
+        if dtype == np.complex128:
+            round_x0 = np.complex128(
+                round(x0.real, num_to_round) + round(x0.imag, num_to_round)*1j)
+            round_convergent_point = np.complex128([round(
+                particular_point.real, num_to_round) + round(particular_point.imag, num_to_round)*1j])
+
+        if round_x0 == round_convergent_point:
+            path_point_convergence_flag = True
+        
+        x0 = z0
+        
+    iterations = 0
+
+
+    # if abs(f(x0)*d2f(x0)) >= abs(df(x0)**2):
     #     print("Newton method fails.")
     #     return None
-        
+
     # df = partial(df_simple, f=f)
     while True:
         # x_axis = np.linspace(-math.pi/3, math.pi/3, 1000)
         # y_axis = np.array([f(i) for i in x_axis])
-        
+
         # tangent_f = get_tangent_f(f, df, x)
         # tangent_y_axis = np.array([tangent_f(i) for i in x_axis], dtype=np.float64)
         # # tangent_y_axis = np.array([-i for i in x_axis])
-        
+
         # plt.axhline(0, color='black', linewidth=0.5)
         # plt.axvline(0, color='black', linewidth=0.5)
-        
+
         # plt.plot(x_axis, y_axis)
         # plt.plot(x_axis, tangent_y_axis)
         # plt.show()
         # print(x)
-        
+
         x = dtype(x) - f(dtype(x)) / df(dtype(x))
         if path_point_convergence_flag:
             convergent_roots.append(x)
-            
+
         if show_convergence:
             print(x)
+
+        iterations += 1
         
         if abs(x - x0) < epsilon:
             break
         x0 = x
-    return x
+    return x, iterations
+
 
 def draw_complex(path_point_convergence_flag=False):
-        # plt.scatter(path_real, path_imag)
+    # plt.scatter(path_real, path_imag)
 
     # x_range = np.linspace(-2, 2, 1000)
     # y_range = np.linspace(-2, 2, 1000)
@@ -141,13 +203,12 @@ def draw_complex(path_point_convergence_flag=False):
 
     for (x, y), _ in np.ndenumerate(Z):
         z0 = Z[x, y]
-        root = newton_method_solution(z_function, z0, dtype=np.complex128)
+        root, _ = newton_method_solution(z_function, z0=z0, dtype=np.complex128)
         roots[x, y] = root
 
     path_real = np.array([])
     path_imag = np.array([])
-    
-    
+
     if path_point_convergence_flag:
         print("Roots: ", convergent_roots)
         path_real = np.array([i.real for i in convergent_roots])
@@ -160,19 +221,22 @@ def draw_complex(path_point_convergence_flag=False):
 
     plt.imshow(colors, extent=[-1, 1, -1, 1])
     plt.colorbar()
-    
-    plt.annotate('start', xy=(path_real[0], path_imag[0]), xytext=(path_real[0]-0.1, path_imag[0]+0.1))
-    plt.annotate('end', xy=(path_real[-1], path_imag[-1]), xytext=(path_real[-1]-0.1, path_imag[-1]+0.1))
-    plt.plot(path_real[-1], path_imag[-1], 'ro')  # Mark the last point (root) with a red circle
+
+    plt.annotate('start', xy=(path_real[0], path_imag[0]), xytext=(
+        path_real[0]-0.1, path_imag[0]+0.1))
+    plt.annotate('end', xy=(
+        path_real[-1], path_imag[-1]), xytext=(path_real[-1]-0.1, path_imag[-1]+0.1))
+    # Mark the last point (root) with a red circle
+    plt.plot(path_real[-1], path_imag[-1], 'ro')
 
     # Annotate the starting point
-    plt.plot(path_real, path_imag, 'bo-', linewidth=2)  # 'bo-' creates blue circles with a line connecting them
+    # 'bo-' creates blue circles with a line connecting them
+    plt.plot(path_real, path_imag, 'bo-', linewidth=2)
     plt.show()
 
+
 if __name__ == "__main__":
-    # print(newton_method_solution(func_to_solve, np.float64(-7.7), dtype=np.float64))
-    # print(bisection_solution(func_to_solve, np.float64(-4.6), np.float64(-4.4)))
-    # print(simple_iteration_solution(func_to_solve, np.float64(0.5)))
-    # print(secant_method_solution(func_to_solve, np.float64(-1), np.float64(1)))
-    # print(newton_method_solution(z_function, complex(0.5,-0.8), dtype=complex, show_convergence=True))
-    draw_complex(path_point_convergence_flag=True)
+    # print("Newton method solution: ", newton_method_solution(func_to_solve, n=-3))  
+    # print("Secant method solution: ", secant_method_solution(func_to_solve, n=-2))
+    # print(bisection_solution(func_to_solve, n=1))
+    print(simple_iteration_solution(func_to_solve, n=0, epsilon=0.1))
--- MVM/lab4/lab4.py
+++ MVM/lab4/lab4.py
@@ -37,6 +37,9 @@ def simpson_integral(f, interval, N_parts=1000):
         a = a + sys.float_info.epsilon
     if b == 1:
         b = b - sys.float_info.epsilon
+        
+    if N_parts % 2 == 0:
+        N_parts -= 1
 
     # f_roots_list = [(f(a), a), (f((a+b)/2), (a+b)/2), (f(b), b)]
 
@@ -57,11 +60,25 @@ def simpson_integral(f, interval, N_parts=1000):
     # plt.plot(interval_array, p_interval_array, label="p(x)")
     # plt.legend()
 
-    h = (b - a) / N_parts
+    h = (b - a) / (N_parts - 1)
+    # print(h, interval_array)
 
-    f_for_int = f
-    integral_result = h/3*(f_for_int(a) + 4*np.sum(np.array([f_for_int(interval_array[2*i - 1]) for i in range(1, N_parts//2 + 1)]))
-                           + 2*np.sum(np.array([f_for_int(interval_array[2*i]) for i in range(1, N_parts//2)])) + f_for_int(b))
+    f_for_int = np.vectorize(f)
+
+    # print(f(a), f(b))
+    array_1 = f_for_int(interval_array[1:N_parts-1:2])
+    # print(array_1)
+    array_2 = f_for_int(interval_array[0:N_parts-2:2])
+    # print(array_2)
+    integral_result = h/3 * \
+        (f_for_int(a) + 4*np.sum(array_1) + 2*np.sum(array_2) + f_for_int(b))
+
+    # another way
+    # left_sum = 9 * f(a) + 28*f(interval_array[1]) + 23*f(interval_array[2])
+    # array = f_for_int(interval_array[3:N_parts-3])
+    # right_sum = 23*f(interval_array[N_parts-3]) + 28*f(interval_array[N_parts-2]) + 9*f(b)
+
+    # integral_result = h/24 * (left_sum + 24*np.sum(array) + right_sum)
 
     # обе подынтегральные функции, данные в задаче,
     # не являются гладкими в области интегрирования,
@@ -71,16 +88,18 @@ def simpson_integral(f, interval, N_parts=1000):
 
     # R = (b - a)*h**4*m_4/2880
 
-    plt.show()
+    # plt.show()
     return integral_result  # , R
 
 
 if __name__ == '__main__':
     # 1 интеграл
-    actual = 8.03491067542
-    res = simpson_integral(func_a, interval_for_a, N_parts=1000000)
-    print(res, abs(res-actual))
-    # 8.03491975440937 9.07898936652884e-6
+    # actual = 8.03491067542
+    # res = simpson_integral(func_a, interval_for_a, N_parts=20)
+    # print(res, abs(res-actual))
+    # 10000 parts, 8.03581849689823 0.000907821478230986
+    # 100000 parts, 8.03500146463434 9.07892143420241e-5
+    # 1000000 parts, 8.03491975440937 9.07898936652884e-6
 
     # замена для 2 интеграла
     def x_t(t):
@@ -89,8 +108,21 @@ if __name__ == '__main__':
     def f_b_with_t(t):
         return func_b(x_t(t))/(1-t)**2
 
-    # 2 интеграл
-    # actual = 2.98100345256
-    # res = simpson_integral(f_b_with_t, interval_for_a, N_parts=1000000)
+    # def f_sin_with_t(t):
+    #     return sympy.sin(t)
+
+    # actual = 1 - math.sqrt(2)/2
+    # res = simpson_integral(f_sin_with_t, (0, math.pi/4), N_parts=10)
     # print(res, abs(res-actual))
-    # 2.98100047163605 2.98092394990235e-6
+    # res = simpson_integral(f_sin_with_t, (0, math.pi/4), N_parts=100)
+    # print(res, abs(res-actual))
+    # res = simpson_integral(f_sin_with_t, (0, math.pi/4), N_parts=1000)
+    # print(res, abs(res-actual))
+
+    # 2 интеграл
+    actual = 2.98100345256
+    actual = 0.266021817462
+    res = simpson_integral(f_b_with_t, (0.3, 0.5), N_parts=1000)
+    print(res, abs(res-actual))
+    res = simpson_integral(f_b_with_t, (0.3, 0.5), N_parts=2000)
+    print(res, abs(res-actual))
