298293bb1f876167a7ccf370c5b0f82359a6c86b
Sergey Novikov <isnovikov0@gmail.com>
2024-05-20 16:11:53 +0700

    NSTT13 fix

--- CPlusPlus/NSTT13/src/main.cpp
+++ CPlusPlus/NSTT13/src/main.cpp
@@ -1,45 +1,49 @@
 #include <iostream>
 
-template<typename T>
-bool isEven(T number) {
+template <typename T>
+bool isEven(T number)
+{
     return number % 2 == 0;
 }
 
 template <typename Checker, typename None = void>
-bool isMatch(Checker checker, int& index) {
+bool isMatch(Checker checker, int &index)
+{
     return false;
 }
 
-template<typename Checker, typename FirstArg, typename... Args>
-bool isMatch(Checker checker, int& index, FirstArg firstArg, Args... args) {
-    // return checker(*firstArg) ? true : isMatch(checker, index + 1, args...);
-    if (checker(*firstArg)) {
+template <typename Checker, typename FirstArg, typename... Args>
+bool isMatch(Checker checker, int &index, FirstArg &&firstArg, Args... args)
+{
+    if (checker(std::forward<FirstArg>(firstArg)))
+    {
         return true;
-    } else {
+    }
+    else
+    {
         index++;
         return isMatch(checker, index, args...);
     }
 }
 
-template<typename Checker, typename... Args>
-int getIndexOfFirstMatch(Checker checker, Args... args){
+template <typename Checker, typename... Args>
+int getIndexOfFirstMatch(Checker checker, Args... args)
+{
     int index = 0;
     return isMatch(checker, index, args...) ? index : -1;
 }
 
-
-
-
-
-int main() {
-    int numbers[] = {5, 6, 7, 8, 10};
-    int index = getIndexOfFirstMatch(isEven<int>, numbers);
-    if (index == -1) {
+int main()
+{
+    int val = 4;
+    int index = getIndexOfFirstMatch(isEven<int>, val, 6, 7, 8);
+    if (index == -1)
+    {
         std::cout << "No even number found" << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "The first even number is at index " << index << std::endl;
     }
     return 0;
 }
-
-
